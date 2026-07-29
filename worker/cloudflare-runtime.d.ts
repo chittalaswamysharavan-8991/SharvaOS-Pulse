0f3aa9d69ad4fd52af05f0ee696e21e3f2a41b09
chittalaswamysharavan-8991 <chittalaswamysharavan@gmail.com>
2026-07-30 03:57:08 +0530

    ci: tighten asset fetcher promise contract

--- worker/cloudflare-runtime.d.ts
+++ worker/cloudflare-runtime.d.ts
@@ -1,7 +1,7 @@
 type D1Database = import("@cloudflare/workers-types").D1Database;
 
 interface Fetcher {
-  fetch(request: Request): Response | Promise<Response>;
+  fetch(request: Request): Promise<Response>;
 }
 
 declare module "cloudflare:workers" {
